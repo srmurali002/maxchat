56eda243cdbc32b01cc0c4effb0a324bfb5c3555
Muralidhar Reddy Challa <srmurali002@gmail.com>
2021-08-20 19:44:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ io.on('connection', (socket) => {
   var addedUser = false;
   var room = null;
   
-  socket.on('roomname',(rm)==>{
+  socket.on('roomname',(rm)=>{
     room = rm;
     socket.join(room);
   });
 
   // when the client emits 'new message', this listens and executes
-  socket.on('new message',(data) ==> {
+  socket.on('new message',(data) => {
     // we tell the client to execute 'new message'
     socket.to(room).emit('new message', {
       username: socket.username,
@@ -37,7 +37,7 @@ io.on('connection', (socket) => {
   });
 
   // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username)==> {
+  socket.on('add user', (username)=> {
     if (addedUser) return;
 
     // we store the username in the socket session for this client
@@ -56,14 +56,14 @@ io.on('connection', (socket) => {
   });
 
   // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', ()==>{
+  socket.on('typing', ()=>{
     socket.to(room).emit('typing', {
       username: socket.username
     });
   });
 
   // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', ()==> {
+  socket.on('stop typing', ()=> {
     socket.to(room).emit('stop typing', {
       username: socket.username
     });
@@ -71,7 +71,7 @@ io.on('connection', (socket) => {
   
 
   // when the user disconnects.. perform this
-  socket.on('disconnect',()==> {
+  socket.on('disconnect',()=> {
     if (addedUser) {
     ro = io.sockets.adapter.rooms[room];
       if(ro){
